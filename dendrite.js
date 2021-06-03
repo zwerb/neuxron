e87828df3ae31b8223b98e2c60f48177f3daef13
Zwerb <sy@zwerb.com>
2021-06-03 12:49:43 -0700

    added classes for axon_terminal, dendrite, neural_circuit

---
+++ dendrite.js
@@ -0,0 +1,46 @@
+/**
+ * @license
+ * Copyright Zwerb LLC. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://raw.githubusercontent.com/zwerb/node_neuron/master/LICENSE
+ */
+
+class Dendrite {
+  static __id = 0;
+
+  constructor(parent_neuron, connected_to = null) {
+    this._id = Dendrite.__id++;
+    this._threshold = 0;
+    this._is_firing = false;
+    this._parent_neuron = parent_neuron;
+    this._connected_to = connected_to;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get threshold() {
+    return this._threshold;
+  }
+
+  get is_firing() {
+    return this._is_firing;
+  }
+
+  get parent_neuron() {
+    return this._parent_neuron;
+  }
+
+  get connected_to() {
+    return this._connected_to;
+  }
+
+  toggle_firing() {
+    this._is_firing = !this._is_firing;
+  }
+
+}
+
+module.exports = Dendrite;
--- neuron.js
+++ neuron.js
@@ -6,15 +6,28 @@
  * found in the LICENSE file at https://raw.githubusercontent.com/zwerb/node_neuron/master/LICENSE
  */
 
+const Axon_Terminal = require("./axon_terminal");
+const Dendrite = require("./dendrite");
+
 class Neuron {
   static __id = 0;
 
   constructor() {
     this._id = Neuron.__id++;
+    this._dendrites = [];
+    this._axon_terminals = [];
   }
 
   get id() {
     return this._id;
+  }
+
+  add_dendrite(connected_to = null){
+    this._dendrites.push(new Dendrite(this,connected_to))
+  }
+
+  add_axon_terminal(connected_to = null){
+    this._axon_terminals.push(new Axon_Terminal(this,connected_to))
   }
 }
 
